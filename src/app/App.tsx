25e291612829b7fcf7a667f007c673f5b08f83a9
naTpuk-ep <naTpuk.ep@gmail.com>
2021-03-12 16:12:03 +0300

    refactor: rename Description component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Route, Switch, useParams } from "react-router-dom";
 import routes from "../constants/routes";
 import "./App.scss";
-import CountryDescription from "./Components/Description";
+import CountryDescription from "../components/CountryDescription";
 
 const MainPage: React.FunctionComponent = () => {
   return <h1>Home</h1>;
