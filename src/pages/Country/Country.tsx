05fabd39a34e69016f670ec762c1a0684fb581d8
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2021-03-13 15:49:53 +0300

    feat: implement getting data from API

--- src/pages/Country/Country.tsx
+++ src/pages/Country/Country.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import axios from "axios";
+import ICountryData from "../../models/country-data";
+import Loader from "../../components/Loader";
 
 interface ICountryPageParams {
   countryId: string;
@@ -8,10 +11,29 @@ interface ICountryPageParams {
 
 const CountryPage: React.FunctionComponent = () => {
   const { countryId } = useParams<ICountryPageParams>();
+  const [isLoad, setIsLoad] = useState(false);
+  const [countryData, setCountryData] = useState<ICountryData>();
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      const result = await axios(
+        `https://rnovikov-travel-app-backend.herokuapp.com/countries/${countryId}`
+      );
+
+      setCountryData(result.data[0]);
+      setIsLoad(true);
+    };
+
+    getCountriesData();
+  }, [countryId]);
 
   return (
     <Container className="country-page">
-      <h1>Country: {countryId}</h1>
+      {isLoad ? (
+        <h1>Country: {countryData?.localizations.en.name}</h1>
+      ) : (
+        <Loader />
+      )}
     </Container>
   );
 };
