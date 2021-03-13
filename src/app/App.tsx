2ac0aad05e21f70fc5bfb53677fd2fbd65611ae4
Raman Novikau <demoikk@gmail.com>
2021-03-14 00:32:59 +0300

    feat: add registration page styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,26 +6,45 @@ import Main from "../pages/Main";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Country from "../pages/Country";
+import useAuth from "../hooks/auth.hook";
+import AuthContext from "../context/AuthContext";
+import Registration from "../pages/Registration";
 
 const App: React.FunctionComponent = () => {
+  const { token, login, logout, userId, ready } = useAuth();
+  const isAuthenticated = !!token;
+
   return (
     <>
-      <header>
-        <Header />
-      </header>
-      <main>
-        <Switch>
-          <Route exact path={routes.HOME}>
-            <Main />
-          </Route>
-          <Route path={routes.COUNTRY}>
-            <Country />
-          </Route>
-        </Switch>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
+      <AuthContext.Provider
+        value={{
+          token,
+          login,
+          logout,
+          userId,
+          isAuthenticated,
+        }}
+      >
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Switch>
+            <Route exact path={routes.HOME}>
+              <Main />
+            </Route>
+            <Route exact path={routes.COUNTRY}>
+              <Country />
+            </Route>
+            <Route exact path={routes.SING_UP}>
+              <Registration />
+            </Route>
+          </Switch>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </AuthContext.Provider>
     </>
   );
 };
