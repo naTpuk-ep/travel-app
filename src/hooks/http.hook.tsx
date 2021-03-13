22acd091ec50c04c8e7e0bd82f46f1073764cf43
Raman Novikau <demoikk@gmail.com>
2021-03-14 00:34:00 +0300

    feat: add http hook

---
+++ src/hooks/http.hook.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+/* eslint-disable no-param-reassign */
+import { useState, useCallback } from "react";
+
+const useHttp = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const request = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setLoading(true);
+      try {
+        if (body) {
+          body = JSON.stringify(body);
+          headers["Content-Type"] = "application/json";
+        }
+
+        const response = await axios({ method, url, data: body, headers });
+
+        const { data } = response;
+
+        setLoading(false);
+        return data;
+      } catch (e) {
+        setLoading(false);
+        setError(e.message);
+        throw e;
+      }
+    },
+    []
+  );
+
+  const clearError = useCallback(() => setError(null), []);
+
+  return { loading, request, error, clearError };
+};
+
+export default useHttp;
