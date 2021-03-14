afc8cb2e8693d2f8b0b777b9eb0e4ec2f5309d59
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2021-03-14 16:11:08 +0300

    feat: add LocalizationContext

---
+++ src/context/LocalizationContext.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import Language from "../constants/languages";
+
+const LocalizationContext = React.createContext(Language.English);
+
+export default LocalizationContext;
