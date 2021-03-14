73708d8d2ea9075bbe8ce2047c38c923fbf0028f
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2021-03-14 11:46:45 +0300

    feat: change logo for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
 import { Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import routes from "../../constants/routes";
 import "./Header.scss";
 
 const Header: React.FunctionComponent = () => {
-  return <Container>Header</Container>;
+  return (
+    <Container className="header">
+      <LinkContainer to={routes.HOME}>
+        <h1 className="header__logo">
+          Travel <span className="header__logo-span">App</span>
+        </h1>
+      </LinkContainer>
+    </Container>
+  );
 };
 
 export default Header;
