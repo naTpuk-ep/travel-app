0bd360156d04be0d9947ee8d91cf5478a290a673
naTpuk-ep <naTpuk.ep@gmail.com>
2021-03-15 03:06:08 +0300

    feat: add Time component

--- src/components/CountryDescription/CountryDescription.tsx
+++ src/components/CountryDescription/CountryDescription.tsx
@@ -2,6 +2,7 @@
 import React, { FC, useContext } from "react";
 import LocalizationContext from "../../context/LocalizationContext";
 import ICountryData from "../../models/country-data";
+import Time from "../Time";
 import "./CountryDescription.scss";
 
 interface ICountryDescriptionProps {
@@ -19,6 +20,7 @@ const CountryDescription: FC<ICountryDescriptionProps> = ({
     localizations: {
       [language]: { capital, name, description },
     },
+    timezones,
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   } = countryData!;
 
@@ -29,7 +31,10 @@ const CountryDescription: FC<ICountryDescriptionProps> = ({
         <img className="description__flag" src={flag} alt={name} />
         <h1 className="description__name">{name}</h1>
       </div>
-      <h2 className="description__capital">{`Capital: ${capital}`}</h2>
+      <div className="description__capital">
+        <h2 className="description__capital__name">{`Capital: ${capital}`}</h2>
+        <Time timezone={timezones[0]} />
+      </div>
       <p className="description__info">{description}</p>
     </div>
   );
--- src/components/Time/Time.tsx
+++ src/components/Time/Time.tsx
@@ -1,7 +1,62 @@
-import React, { FC } from "react";
+import React, { FC, useCallback, useEffect, useState } from "react";
 
-const Time: FC = () => {
-  return <div className="time">time</div>;
+interface ITime {
+  date: string;
+  month: string;
+  year: string;
+  hour: string;
+  minute: string;
+}
+
+interface ITimeProps {
+  timezone: string;
+}
+
+const Time: FC<ITimeProps> = ({ timezone }: ITimeProps) => {
+  const addZero = (number: number) => `${number < 10 ? "0" : ""}${number}`;
+  const clockTimeZone = useCallback(() => {
+    const timeZoneValues = timezone.match(/^(?:UTC\+)(\d{2})/);
+    const timezoneOffset = +(timeZoneValues ? timeZoneValues[1] : 0);
+    const localTime = new Date();
+    const ms =
+      localTime.getTime() +
+      localTime.getTimezoneOffset() * 60000 +
+      timezoneOffset * 3600000;
+    const timeOfZone = new Date(ms);
+
+    const hour = addZero(timeOfZone.getHours());
+    const minute = addZero(timeOfZone.getMinutes());
+    const date = addZero(timeOfZone.getDate());
+    const month = addZero(timeOfZone.getMonth() + 1);
+    const year = addZero(timeOfZone.getFullYear());
+    return {
+      hour,
+      minute,
+      date,
+      month,
+      year,
+    };
+  }, [timezone]);
+
+  const [time, setTime] = useState<ITime>(clockTimeZone());
+
+  useEffect(() => {
+    let timeOut: NodeJS.Timeout;
+    const getTime = () => {
+      setTime(clockTimeZone());
+      timeOut = setTimeout(getTime, 1000);
+    };
+    getTime();
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [clockTimeZone]);
+  const { hour, minute, date, month, year } = time;
+  return (
+    <div className="time">
+      <h5>{`Local time(${timezone}): ${hour}:${minute} ${date}-${month}-${year}`}</h5>
+    </div>
+  );
 };
 
 export default Time;
