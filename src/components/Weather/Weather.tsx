9789dcf1ada32e68e2dee8fbfc718a552c177f3c
naTpuk-ep <naTpuk.ep@gmail.com>
2021-03-16 20:47:50 +0300

    fix: fix weatherWidgete for localization

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,36 +1,50 @@
+import axios from "axios";
 import React, { createRef, FC, useContext, useEffect, useState } from "react";
-import ReactWeather, { useOpenWeather } from "react-open-weather";
+// import ReactWeather, { useOpenWeather } from "react-open-weather";
 import LocalizationContext from "../../context/LocalizationContext";
 import ICountryData from "../../models/country-data";
+import Loader from "../Loader";
 import "./Weather.scss";
 
 interface IWeatherProps {
   countryData: ICountryData | undefined;
 }
 
+interface IWeatherData {
+  temp: number;
+  id: string;
+  humidity: number;
+  wind: {
+    deg: number;
+    speed: number;
+  };
+}
+
 const Weather: FC<IWeatherProps> = ({ countryData }: IWeatherProps) => {
   const language = useContext(LocalizationContext);
   const APIKey = "ce37a86730058fe8d88e2bd624a9e06f";
-  const { data, isLoading, errorMessage } = useOpenWeather({
-    key: APIKey,
-    lat: countryData?.capitalLocation.coordinates[0],
-    lon: countryData?.capitalLocation.coordinates[1],
-    lang: language,
-    unit: "metric",
-  });
-  const customStyles = {
-    fontFamily: "Helvetica, sans-serif",
-    gradientStart: "white",
-    gradientMid: "white",
-    gradientEnd: "white",
-    locationFontColor: "black",
-    todayTempFontColor: "black",
-    todayDateFontColor: "black",
-    todayRangeFontColor: "black",
-    todayDescFontColor: "black",
-    todayInfoFontColor: "black",
-    todayIconColor: "black",
-  };
+  const [weatherData, setWeatherData] = useState<IWeatherData | undefined>();
+  // const weatherData = useOpenWeather({
+  //   key: APIKey,
+  //   lat: countryData?.capitalLocation.coordinates[0],
+  //   lon: countryData?.capitalLocation.coordinates[1],
+  //   lang: language,
+  //   unit: "metric",
+  // });
+  // const { data, isLoading, errorMessage } = weatherData;
+  // const customStyles = {
+  //   fontFamily: "Helvetica, sans-serif",
+  //   gradientStart: "white",
+  //   gradientMid: "white",
+  //   gradientEnd: "white",
+  //   locationFontColor: "black",
+  //   todayTempFontColor: "black",
+  //   todayDateFontColor: "black",
+  //   todayRangeFontColor: "black",
+  //   todayDescFontColor: "black",
+  //   todayInfoFontColor: "black",
+  //   todayIconColor: "black",
+  // };
   const [minimized, setMinimized] = useState("minimized");
   const clickHandler = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -50,6 +64,29 @@ const Weather: FC<IWeatherProps> = ({ countryData }: IWeatherProps) => {
     return () => window.removeEventListener("click", closeWidgetes);
   });
 
+  useEffect(() => {
+    const getWeather = async () => {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${countryData?.localizations[language].capital}&lang=${language}&appid=${APIKey}&units=metric`
+      );
+      const {
+        main: { temp, humidity },
+        weather: [{ id }],
+        wind,
+      } = res.data;
+      setWeatherData({
+        temp,
+        humidity,
+        id,
+        wind,
+      });
+    };
+
+    getWeather();
+  }, [countryData, language]);
+
+  console.log(weatherData ?? "noWdata");
+
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events
     <div
@@ -59,7 +96,7 @@ const Weather: FC<IWeatherProps> = ({ countryData }: IWeatherProps) => {
       onClick={clickHandler}
       className={`weather ${minimized}`}
     >
-      <ReactWeather
+      {/* <ReactWeather
         isLoading={isLoading}
         errorMessage={errorMessage}
         data={data}
@@ -68,7 +105,21 @@ const Weather: FC<IWeatherProps> = ({ countryData }: IWeatherProps) => {
         unitsLabels={{ temperature: "℃", windSpeed: "Km/h" }}
         showForecast={false}
         theme={customStyles}
-      />
+      /> */}
+      {weatherData ? (
+        <>
+          <div className="weather__left">
+            <h3>{weatherData.temp}</h3>
+            <br />
+            {/* <p>{weatherData.de}</p> */}
+          </div>
+          <div className="weather__icon">
+            <i className={`owf owf-${weatherData.id}-d owf-5x`} />
+          </div>
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
