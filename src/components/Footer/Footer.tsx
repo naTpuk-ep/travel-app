3492450382eaf9465338171046b52a75c9fc7c12
naTpuk-ep <naTpuk.ep@gmail.com>
2021-03-17 18:22:25 +0300

    feat: links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,48 @@
 import * as React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Nav } from "react-bootstrap";
+import LOCALIZATIONS from "../../assets/data/localizations";
+import LocalizationContext from "../../context/LocalizationContext";
 import "./Footer.scss";
 
 const Footer: React.FunctionComponent = () => {
+  const language = React.useContext(LocalizationContext);
   return (
     <Container>
-      <div className="footer">Footer</div>
+      <div className="footer">
+        {/* <h5>{`${LOCALIZATIONS.authors[language]}:`} </h5> */}
+        {/* <ul>
+          <li>
+            <a href="https://github.com/DmitryAstapenko">DmitryAstapenko</a>
+            <a href="https://github.com/RamanNovikau">RamanNovikau</a>
+            <a href="https://github.com/naTpuk-ep">YauheniPiatrou</a>
+            <a href="https://github.com/zhenyamarinovich">ZhenyaMarinovich</a>
+          </li>
+        </ul> */}
+        <Nav className="mr-auto">
+          <h5 className="authors">{`${LOCALIZATIONS.authors[language]}:`}</h5>
+          &nbsp; &nbsp;
+          <Nav.Link href="https://github.com/DmitryAstapenko">
+            DmitryAstapenko
+          </Nav.Link>
+          <Nav.Link href="https://github.com/RamanNovikau">
+            RamanNovikau
+          </Nav.Link>
+          <Nav.Link href="https://github.com/naTpuk-ep">
+            YauheniPiatrou
+          </Nav.Link>
+          <Nav.Link href="https://github.com/zhenyamarinovich">
+            ZhenyaMarinovich
+          </Nav.Link>
+        </Nav>
+        <div className="logo">
+          <a href="https://rs.school/js/">
+            <img
+              src="https://rs.school/images/rs_school_js.svg"
+              alt="RS-school"
+            />
+          </a>
+        </div>
+      </div>
     </Container>
   );
 };
